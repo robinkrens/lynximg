e99cf42eaf651b8f44b054a8442be31c7022f9d9
Robin Krens <robin@robinkrens.nl>
2020-07-17 00:44:18 +0200

    basic line reading and structing

--- packit.c
+++ packit.c
@@ -30,6 +30,46 @@
 #define MAX_COLORS 256
 #define VERBOSE 0
 
+typedef struct packed_data {
+	int repeatcount;
+	uint8_t color; /* Palette color 0-16 */
+	struct packed_data * next;
+} packed_data_t;
+
+packed_data_t * scan_line(uint8_t * data, int data_len) 
+{
+	struct packed_data * top;
+	struct packed_data * current = malloc(sizeof(packed_data_t));
+
+	top = current;
+
+	current->repeatcount = 1;
+	current->color = *data;
+	current->next = NULL;
+
+	data++;
+
+	for (int i = 0; i < data_len-1; i++) {
+		
+		if(*data == current->color) {
+			current->repeatcount++;
+		}
+		else {
+			current->next = malloc(sizeof(packed_data_t));
+			current = current->next;
+			current->repeatcount = 1;
+			current->color = *data;
+			current->next = NULL;
+		}
+
+		data++;
+	}
+	
+	return top;
+
+}
+
+
 int print_image_stats(SDL_Surface * img)
 {
 
@@ -53,29 +93,96 @@ int check_unique(int color, int * palette, int current_count)
 	return 1;
 }
 
+int get_paletteindex(int color, int * palette, int current_count) 
+{
+	for (int idx = 0; idx < current_count; idx++) {
+		if (palette[idx] == color)
+			return idx;
+	}
+	fprintf(stderr, "can't find color");
+	return -1;
+}
+
+int data_packet_line(int rowlength, uint8_t * data, int data_len)
+{
+
+	int offset = 0;
+	uint8_t * top;
+	uint8_t * linesprite = (uint8_t *) malloc(256);
+	bzero(linesprite, 0);
+
+	top = linesprite;
+
+	/* offset to next line of sprite */
+	*linesprite = rowlength/2 + 3;
+	linesprite++;
+	offset++;
+
+	/* first bit: literal */
+	*linesprite = 1 << 7;
+
+	/* number of pixels + 1 (rowlength) */
+	*linesprite |= rowlength-1 << 3;
+
+	//linesprite++;
+	//offset++;
+
+	/* pixel data */
+	for (int i = 0; i < data_len;) {
+		//*linesprite = *data << 7;
+		*linesprite |= *data >> 1;
+		linesprite++;
+		offset++;
+
+		*linesprite = (*data & 0x01) << 7;
+
+		data++;
+
+		if (i + 1 < data_len) {
+			*linesprite |= *data << 3;
+		//	linesprite++;
+		//	offset++;
+		}
+
+		data++;
+		
+		i += 2;
+	}
+
+	/* padding */
+	//linesprite++;
+	offset++;
+
+	for (int i = 0; i < offset; i++)
+		printf("%0X:", top[i]);
+
+	return 0;
+}
+
 int main() 
 {
-	SDL_Surface * bmpfile;
+
+	SDL_Surface * rawbmp;
 	SDL_Color * color;
 	SDL_PixelFormat * fmt;
 	int index_r, index_g, index_b;
 	int colorcnt = 0;
 	int palettebuf[MAX_COLORS];
 
-	bmpfile = SDL_LoadBMP("brick.bmp");
+	rawbmp = SDL_LoadBMP("brick.bmp");
 
-	if(!bmpfile) {
+	if(!rawbmp) {
 		fprintf(stderr, "Can't load bmp file!\n");
 		return -1;
 	}
 
-	if(bmpfile->format->BitsPerPixel != 24) {
-		fprintf(stderr, "Bits per pixel: %d not supported\n", bmpfile->format->BitsPerPixel);
+	if(rawbmp->format->BitsPerPixel != 24) {
+		fprintf(stderr, "Bits per pixel: %d not supported\n", rawbmp->format->BitsPerPixel);
 		return -1;
 	}
 
-	print_image_stats(bmpfile);
-	fmt = bmpfile->format;
+	print_image_stats(rawbmp);
+	fmt = rawbmp->format;
 
 	if (fmt->palette) {
 		fprintf(stderr, "Can't handle palette BMP!\n");
@@ -83,18 +190,22 @@ int main()
 	}
 
 	/* Padding; currently only 24-bit (3 byte) supported  */
-	int padding = ceil((float)bmpfile->w * 3/4) * 4 - (bmpfile->w * 3);
+	int padding = ceil((float)rawbmp->w * 3/4) * 4 - (rawbmp->w * 3);
 
-	for (int h = 0; h < bmpfile->h; h++) {
+	for (int h = 0; h < rawbmp->h; h++) {
 
-		for (int w = 0; w < bmpfile->w; w++) {
+		uint8_t * top;
+		uint8_t * linebuf = (uint8_t *) malloc(rawbmp->w);
+		top = linebuf;
+		
+		for (int w = 0; w < rawbmp->w; w++) {
 
-			SDL_LockSurface(bmpfile);
+			SDL_LockSurface(rawbmp);
 			/* 24-bit pixel data are loaded as blue, green, red */
-			index_b = * (uint8_t * ) bmpfile->pixels++;
-			index_g = * (uint8_t * ) bmpfile->pixels++;
-			index_r = * (uint8_t * ) bmpfile->pixels++;
-			SDL_UnlockSurface(bmpfile);
+			index_b = * (uint8_t * ) rawbmp->pixels++;
+			index_g = * (uint8_t * ) rawbmp->pixels++;
+			index_r = * (uint8_t * ) rawbmp->pixels++;
+			SDL_UnlockSurface(rawbmp);
 
 			/* Check unique color */
 			if (check_unique(((index_b << 16) | (index_g << 8) | index_r), palettebuf, colorcnt)) {
@@ -110,14 +221,57 @@ int main()
 			
 			if (VERBOSE)
 				fprintf(stdout, "height:width: %d:%d, B-G-R: %d, %d, %d\n", h, w, index_b, index_g, index_r);
+
+			int idx = get_paletteindex(((index_b << 16) | (index_g << 8) | index_r), palettebuf, colorcnt);
+			//printf("idx: %d\n", idx);
+			*linebuf++ = idx;
+			
 		}
 
-		bmpfile->pixels += padding;
+		packed_data_t * prev;
+		packed_data_t * l = scan_line(top, rawbmp->w);
+
+		printf("new line\n");
+
+		while(l->next != NULL) {
+			printf("repeat: %d, color: %x\n", l->repeatcount, l->color);
+			prev = l;	
+			l = l->next;
+			free(prev);	
+		}
+		printf("repeat: %d, color: %x\n", l->repeatcount, l->color);
+		free(l);
+		free(top);
+
+		rawbmp->pixels += padding;
 	}
 		
 	fprintf(stdout, "Image has %d colors\n", colorcnt);
+	
 
-	//SDL_FreeSurface(bmpfile);
+//	uint8_t buff[8];
+//	buff[0] = 0xAA;
+//	buff[1] = 0x22;
+//	buff[2] = 0x22;
+//	buff[3] = 0x33;
+//	buff[4] = 0x33;
+//	buff[5] = 0x33;
+//	buff[6] = 0x33;
+//	buff[7] = 0xAA;
+
+
+
+//	uint8_t buf[5];
+//
+//	buf[0] = 0x0F;
+//	buf[1] = 0x0F;
+//	buf[2] = 0x0F;
+//	buf[3] = 0x0F;
+//	buf[4] = 0x0F;
+//
+//	data_packet_line(5, buf, 5);
+//
+	//SDL_FreeSurface(rawbmp);
 
 	return 0;
 }
